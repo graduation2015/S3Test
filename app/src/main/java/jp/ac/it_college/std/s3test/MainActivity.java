d2858a459bad4814d47a43d025d4b9ffde16beef
s13012 <s13012@std.it-college.ac.jp>
2015-10-09 14:38:09 +0900

    toolbarとフラグメント実装

--- app/src/main/java/jp/ac/it_college/std/s3test/MainActivity.java
+++ app/src/main/java/jp/ac/it_college/std/s3test/MainActivity.java
@@ -1,8 +1,15 @@
 package jp.ac.it_college.std.s3test;
 
+import android.app.Fragment;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
     private String[] mPlanetTitles;
     private ListView mDrawerList;
     private DrawerLayout mDrawerLayout;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,21 +30,108 @@ public class MainActivity extends AppCompatActivity {
 
         findViews();
         setUpDrawerList();
+        setUpToolbar();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void findViews() {
         mPlanetTitles = getResources().getStringArray(R.array.planets_array);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.list_view);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
 
+    private void setUpToolbar() {
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     private void setUpDrawerList() {
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar
+                ,R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                //ドロワーを開いた時に呼ばれる
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                //ドロワーを閉じた時に呼ばれる
+
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
         // Set the adapter for the list view
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, mPlanetTitles));
         // Set the list's click listener
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
+    }
+
+    private class DrawerItemClickListener implements android.widget.AdapterView.OnItemClickListener {
+
+        public static final int FRAGMENT_S3_UPLOAD = 0;
+        public static final int FRAGMENT_S3_DOWNLOAD = 1;
+        public static final int FRAGMENT_S3_LIST = 2;
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            selectItem(i);
+        }
+
+        private void selectItem(int position) {
+            Fragment fragment = getFragment(position);
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container_content, fragment)
+                    .commit();
+
+            mDrawerList.setItemChecked(position, true);
+            setTitle(mPlanetTitles[position]);
+            mDrawerLayout.closeDrawers();
+        }
+
+        private Fragment getFragment(int position) {
+            switch (position) {
+                case FRAGMENT_S3_UPLOAD:
+                    return new S3UploadFragment();
+                case FRAGMENT_S3_DOWNLOAD:
+                    return new S3DownloadFragment();
+                case FRAGMENT_S3_LIST:
+                    return new S3ListFragment();
+                default:
+                    return new S3UploadFragment();
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/jp/ac/it_college/std/s3test/S3ListFragment.java
@@ -0,0 +1,30 @@
+package jp.ac.it_college.std.s3test;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class S3ListFragment extends Fragment {
+
+
+    public S3ListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_s3_list, container, false);
+    }
+
+
+}
