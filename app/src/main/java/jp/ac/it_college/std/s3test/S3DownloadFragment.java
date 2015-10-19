fb37d05f1af2effa289b447ff682c002b7196cc5
s13012 <s13012@std.it-college.ac.jp>
2015-10-19 16:00:36 +0900

    ダウンロード履歴画面完了

--- app/src/main/java/jp/ac/it_college/std/s3test/S3DownloadFragment.java
+++ app/src/main/java/jp/ac/it_college/std/s3test/S3DownloadFragment.java
@@ -1,29 +1,42 @@
 package jp.ac.it_college.std.s3test;
 
 
+import android.app.ListFragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class S3DownloadFragment extends Fragment {
+public class S3DownloadFragment extends ListFragment {
 
-
-    public S3DownloadFragment() {
-        // Required empty public constructor
-    }
-
+    private List<Bitmap> items = new ArrayList<>();
+    private static final String DIRECTORY_PATH = Environment.getExternalStorageDirectory()
+            + "/SampleDirectory/";
+    private String[] list = new File(DIRECTORY_PATH).list();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        setListAdapter(new S3DownloadsListAdapter(getActivity(), R.layout.row_s3_downloads, items));
+        setDownLoads();
         return inflater.inflate(R.layout.fragment_s3_download, container, false);
+    }
+
+    private void setDownLoads() {
+        for (String name : list) {
+            Bitmap bitmap = BitmapFactory.decodeFile(DIRECTORY_PATH + name);
+            items.add(bitmap);
+        }
+
+        ((S3DownloadsListAdapter) getListAdapter()).notifyDataSetChanged();
     }
 
 
